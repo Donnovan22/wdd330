bb64982e340acc2d5bce43bf902ac263dc421f3f
Donnovan22 <val22002@byui.edu>
2022-09-17 16:49:36 -0500

    css implemented

--- js/main.js
+++ js/main.js
@@ -3,30 +3,14 @@ const ol = document.querySelector(".list");
 const links = [
   {
     label: "Week01",
-    url: "week01/index.html"
-  },
-  {
-    label: "Week02",
-    url: "week02/index.html"
-  },
-  {
-    label: "Week03",
-    url: "week03/index.html"
-  },
-  {
-    label: "Week04",
-    url: "week04/index.html"
-  },
-  {
-    label: "Week05",
-    url: "week05/index.html"
+    url: "week01/story_editor.html"
   }
 ]
 
 function addElementToOl() {
   links.forEach(element => {
     const li = document.createElement("li");
-    li.innerHTML = `<a targer=_blank href="${element.url}">${element.label}</a>`;
+    li.innerHTML = `<a targer=_blank href="${element.url}">${element.label}</a>`;  
     ol.append(li);
   });
 };
