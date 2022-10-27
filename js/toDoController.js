46ae65f760cafd4dbe1a2c59891bb9f1bdbe9402
Emanuel <emanuel@Emanuels-MacBook-Air.local>
2022-10-27 02:39:55 -0500

    delete and toggle functions implemented plus some css changes

--- js/toDoController.js
+++ js/toDoController.js
@@ -10,17 +10,12 @@ export default class ToDoController {
     
     showToDoList(){
         this.readFromLocalStorage(); 
-        this.saveToDoModel();
-        //localStorage.clear();
+        this.addToDoElement();
+        this.toDoListInteraction();
     }
 
     showFooter(){
-        // pending..
-    }
-
-    writeToLocalStorage(toDoList) {
-        localStorage.setItem("toDoList", JSON.stringify(toDoList));
-        this.toDoView.renderToDoList(toDoList, this.parentElement);
+        this.toDoView.renderFooter(this.parentElement);
     }
 
     readFromLocalStorage(){
@@ -28,39 +23,68 @@ export default class ToDoController {
         if (array){
             this.toDoList = JSON.parse(array);
             this.toDoView.renderToDoList(this.toDoList, this.parentElement);
+            this.showFooter();
         }
     }
 
-    addToDoModel(text){
+    writeToLocalStorage(toDoList) {
+        localStorage.setItem("toDoList", JSON.stringify(toDoList));
+        this.toDoView.renderToDoList(toDoList, this.parentElement);
+        this.showFooter();
+    }
+
+    saveToDoModel(text){
         let toDoModel = new ToDoModel(Date.now(), text, false);
         this.toDoList.push(toDoModel);
         this.writeToLocalStorage(this.toDoList);
         console.log(toDoModel);
     }
 
-    saveToDoModel(){
+    addToDoElement(){
         const button = document.querySelector("#addButton");
         button.addEventListener("click", event => {
             event.preventDefault();
             const userInput = document.querySelector("#userInput");
             const text = userInput.value.trim();
             if(text !== ""){
-                this.addToDoModel(text);
+                this.saveToDoModel(text);
                 userInput.value = "";    
                 userInput.focus();        
             }});
     }
 
-    completeToDoElement(){
+    toDoListInteraction(){
+        this.parentElement.addEventListener("click", event =>{
+            if (event.target.type === "checkbox") {this.toggle(event.target.parentElement.getAttribute("data-key"));}
+            if (event.target.classList.contains("deleteButton")) {this.removeToDoElement(event.target.parentElement.getAttribute("data-key"));}
+        });
+    }
+
+    filterActiveTasks(){
         // pending..
     }
 
-    removeToDoElement(){
+    filterCompletedTasks(){
         // pending..
     }
 
-    filterToDoList(){
+    filterAllToDoElements(){
         // pending..
     }
 
+    removeToDoElement(id){
+        this.toDoList = this.toDoList.filter(function(toDoModel) {
+            return toDoModel.id != id;
+          });
+        this.writeToLocalStorage(this.toDoList);
+    }
+
+    toggle(id) {
+        this.toDoList.forEach(function(toDoModel) {
+            if (toDoModel.id == id) {
+              toDoModel.bolean = !toDoModel.bolean;
+            }
+          });
+        this.writeToLocalStorage(this.toDoList);
+    }
 }
--- js/toDoView.js
+++ js/toDoView.js
@@ -5,19 +5,41 @@ export default class ToDoView {
 
     renderToDoList(toDoList, parentElement){
         parentElement.innerHTML = "";
-        toDoList.forEach(toDoModel => {          
-            parentElement.appendChild(this.renderOneToDoElement(toDoModel));
+        toDoList.forEach(toDoModel => {   
+            const checked = toDoModel.bolean ? 'checked': null;       
+            parentElement.appendChild(this.renderOneToDoElement(toDoModel, checked));
         });
     }
 
-    renderOneToDoElement(toDoModel){
+    renderOneToDoElement(toDoModel, checked){
         const toDoElement = document.createElement("li");
+        toDoElement.setAttribute("class", "toDoElement");
+        toDoElement.setAttribute("data-key", toDoModel.id);
+
+        if (toDoModel.bolean === true) {
+            toDoElement.classList.add("checked");
+        }
+
         toDoElement.innerHTML = `
-        <input type="checkbox" id="${toDoModel.id}">
+        <input type="checkbox" class="checkbox" ${checked}>
         <span>${toDoModel.text}</span>
-        <button id="deleteToDoElement">Delete</button>
+        <button class="deleteButton">X</button>
         `;
 
         return toDoElement;
     }
-}
+    
+    renderFooter(parentElement){
+        const footer = document.createElement("div");
+        footer.setAttribute("class", "footer-div");
+        footer.innerHTML = `
+        <span id="counter">- tasks left</span>
+        <button id="all">All</button>
+        <button id="active">Active</button>
+        <button id="completed">Completed</button>
+        `;
+
+        parentElement.appendChild(footer);
+    }
+
+  }
